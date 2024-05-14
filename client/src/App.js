c2db0923bbb1f6b67a2ad18f376c111b065daa2c
ArthurGaleev <arturiusgal@gmail.com>
2024-05-14 15:18:16 +0300

    Final Project

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 
 import React, { useState, useEffect } from 'react';
 import { io } from 'socket.io-client';
+import { ToastContainer, toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css'
 
 const socket = io('http://localhost:3001')
 
@@ -10,10 +12,23 @@ function App() {
     const [playerName, setPlayerName] = useState(null);
 
     const [joinedRoom, setJoinedRoom] = useState(false);
-    const [ready, setReady] = useState(false);
 
     const [startGame, setStartGame] = useState(false);
     const [players, setPlayers] = useState([]);
+    const [currentPlayerIndex, setCurrentPlayerIndex] = useState(0);
+
+    const [question, setQuestion] = useState(null);
+    const [questionIndex, setQuestionIndex] = useState(0);
+    const [answer, setAnswer] = useState(null);
+    const [answerSubmitted, setAnswerSubmitted] = useState(false);
+    const [timer, setTimer] = useState(30);
+    const [points, setPoints] = useState(0);
+    const [bank, setBank] = useState(0);
+    const [bankSubmitted, setBankSubmitted] = useState(false);
+    const [round, setRound] = useState(1);
+
+    const [kicked, setKicked] = useState(false);
+    const [won, setWon] = useState(false);
 
 
     const roomIdHandler = (e) => {
@@ -22,9 +37,7 @@ function App() {
     const nameHandler = (e) => {
         setPlayerName(e.target.value);
     }
-    const submitHandler = (e) => {
-        // e.preventDefault()
-
+    const submitFormHandler = (e) => {
         if (roomId && playerName) {
             setJoinedRoom(true);
         }
@@ -32,29 +45,227 @@ function App() {
 
     const renderPlayersBoxes = () => {
         const playerBoxes = [];
-        for (let i = 0; i < players.length; ++i) {
+        for (let i = 0; i < 8; ++i) {
             const player = players[i];
             playerBoxes.push(
-                <div key={player.id} className='PlayerBox'>
+                <div key={i} className='PlayerBox'>
                     {player ? (
-                        <div>{player.name}</div>
+                        <div className='PlayerNameBox'> {player.name}</div>
                     ) : (
-                        <div> Empty slot </div>
+                        <div className='EmptyName'> Empty slot </div>
                     )}
                 </div>
             )
         }
         return playerBoxes;
     }
+    const renderQuestion = (question) => {
+        const submitBox = [];
+        if (kicked && !startGame && won && !playerName) {
+            return;
+        }
 
-    const waitPlayers = () => {
+        submitBox.push(
+            <div className='submitBox'>
+                <div className='Question'> {question} </div>
+
+                {(playerName === players[currentPlayerIndex].name) ? (
+                    <div>
+                        <input required placeholder='your answer' onChange={answerHandler}/>
+                        <button onClick={submitAnswerHandler} type='submit' className='AnswerButton'> submit</button>
+                        <button onClick={submitBankHandler} type='submit' className='BankButton'> bank</button>
+                    </div>
+                ) : (
+                    <div></div>
+                )}
+
+            </div>
+        )
+        return submitBox;
     }
+
 
     useEffect(() => {
         if (joinedRoom) {
-            socket.emit('JoinRoom', roomId, playerName);
+            socket.emit('joinRoom', roomId, playerName);
         }
     }, [joinedRoom]);
+
+    useEffect(() => {
+        socket.on('message', (message) => {
+            // toast(`${message}`, {
+            //     position: 'top-right',
+            //     autoClose: 3000,
+            // })
+        })
+        return () => {
+            socket.off('message');
+        }
+    }, []);
+
+    useEffect(() => {
+        socket.on('setPlayers', (players) => {
+            setPlayers(players);
+        });
+        return ()=> {
+            socket.off('setPlayers');
+        }
+    }, []);
+    useEffect(() => {
+        socket.on('setCurrentPlayerIndex', (currentPlayerIndex) => {
+            setCurrentPlayerIndex(currentPlayerIndex)
+        });
+        return ()=> {
+            socket.off('setCurrentPlayerIndex');
+        }
+    }, []);
+    useEffect(() => {
+        socket.on('setRound', (round) => {
+            setRound(round);
+        });
+        return ()=> {
+            socket.off('setRound');
+        }
+    }, []);
+
+
+    useEffect(() => {
+
+        socket.on('startGame', () => {
+            setStartGame(true);
+        });
+
+        // socket.on('playerEliminated', (playerId) => {
+        //     const newPlayers = players.filter((player) => player.id !== playerId);
+        //     setPlayers(newPlayers);
+        // });
+        //
+        // socket.on('gameOver', (winnerId) => {
+        //     alert("Game over! The winner is ${winnerId}");
+        // });
+        return ()=> {
+            socket.off('startGame');
+        }
+    }, [players]);
+
+
+
+    useEffect(() => {
+        if (timer === 0) {
+            // nextRound
+
+            if (!kicked && startGame && !won && playerName === players[currentPlayerIndex].name) {
+
+                setQuestion(null);
+                socket.emit('nextRound', roomId);
+            }
+
+            // if (!players) {
+            //     return;
+            // }
+            // if (playerName === players[currentPlayerIndex].name) {
+            //     setRound(prevState => prevState + 1);
+            //     setQuestion(null);
+            //     socket.emit('nextRound', roomId);
+            // }
+            return;
+        }
+
+        const interval = setInterval(() => {
+            setTimer(prevState => prevState - 1);
+        }, 1000);
+
+        return () => {
+            clearInterval(interval);
+        }
+    }, [timer]);
+
+
+    useEffect(() => {
+        socket.on('setTimer', (roundTime) => {
+            setTimer(roundTime);
+        });
+        return ()=> {
+            socket.off('setTimer');
+        }
+    }, []);
+    useEffect(() => {
+        socket.on('setPoints', (points) => {
+            setPoints(points);
+        });
+        return ()=>{
+            socket.off('setPoints');
+        }
+    }, []);
+    useEffect(() => {
+        socket.on('setBank', (bank) => {
+            setBank(bank);
+        });
+        return ()=>{
+            socket.off('setBank');
+        }
+    }, []);
+
+
+    useEffect(() => {
+        socket.on('nextQuestion', (question, questionIndex) => {
+
+            setQuestion(question);
+            setQuestionIndex(questionIndex);
+            setAnswer(null);
+            setAnswerSubmitted(false);
+        })
+        return ()=> {
+            socket.off('nextQuestion');
+        }
+    }, [round]);
+
+    useEffect(() => {
+        if (answerSubmitted) {
+            if (playerName === players[currentPlayerIndex].name) {
+                socket.emit('checkAnswer', roomId, currentPlayerIndex, questionIndex, answer);
+            }
+        }
+    }, [answerSubmitted, currentPlayerIndex, questionIndex]);
+
+    useEffect(() => {
+        if (bankSubmitted) {
+            if (playerName === players[currentPlayerIndex].name) {
+                socket.emit('bank', roomId);
+            }
+        }
+    }, [bankSubmitted]);
+
+    useEffect(() => {
+        socket.on('kickTheWeakest', (roomId) => {
+            setKicked(true);
+        })
+        return ()=>{
+            socket.off('kickTheWeakest')
+        }
+    }, [players]);
+    useEffect(() => {
+        socket.on('won', () => {
+            setWon(true);
+        })
+        return ()=>{
+            socket.off('won')
+        }
+    }, []);
+
+
+    const answerHandler = (e) => {
+        setAnswer(e.target.value);
+    }
+    const submitAnswerHandler = (e) => {
+        if (answer) {
+            setAnswerSubmitted(true);
+        }
+    };
+
+    const submitBankHandler = (e) => {
+        setBankSubmitted(true);
+    };
 
 
 
@@ -65,25 +276,57 @@ function App() {
                 <div className='JoinRoom'>
                     <h1> The Weakest Link </h1>
 
-                    <form onSubmit={submitHandler}>
+                    <form onSubmit={submitFormHandler}>
                         <input required placeholder='Room ID' onChange={roomIdHandler}/>
                         <input required placeholder='Name' onChange={nameHandler}/>
-
-                        <button type='submit' className='JoinButton'> join </button>
+                        <button type='submit' className='JoinButton'> join</button>
                     </form>
 
                 </div>
             ) : (
                 <div className='Quiz'>
-                    <div className='PlayersBoxes'>
-                        {renderPlayersBoxes()}
-                    </div>
+                    <div className='container'>
+                        <div className='left-column'>
+                            <div className='PlayersBoxes'>
+                                {renderPlayersBoxes()}
+                            </div>
+                        </div>
+                        <div className='right-column'>
+                            <div className='Title'> The Weakest Link </div>
+                            <div className='RoomId'> Room ID: {roomId} </div>
+                            <div className='PlayerName'> Name: {playerName} </div>
+                            {/*<ToastContainer />*/}
 
-                    {!startGame ? (
-                        waitPlayers()
-                    ) : (
-                        <h1> The Weakest Link </h1>
-                    )}
+                            <div className='StartGame'>
+                                {startGame ? (
+                                    <div className='Game'>
+                                        <div className='Round'> Round: {round}</div>
+                                        <div className='Points'> points: {points} </div>
+                                        <div className='Bank'> bank: {bank} </div>
+
+                                        <div className='KickOrNot'>
+                                            {!kicked ? (
+                                                <div className='WinOrNot'>
+                                                    {!won ? (
+                                                        <div className='Question'>
+                                                            <div className='Timer'> Time remaining: {timer} </div>
+                                                            {renderQuestion(question)}
+                                                        </div>
+                                                        ) : (
+                                                        <div className='YouWon'> You won {bank} points bank! </div>
+                                                    )}
+                                                </div>
+                                            ) : (
+                                                <div className='YouLost'> You lost! </div>
+                                            )}
+                                        </div>
+                                    </div>
+                                ) : (
+                                    <div className='WaitingPlayers'> Waiting players </div>
+                                )}
+                            </div>
+                        </div>
+                    </div>
                 </div>
             )}
         </div>
--- server/index.js
+++ server/index.js
@@ -32,44 +32,157 @@ const questions = [
         question: 'Вопрос 4',
         answer: 'Ответ 4'
     },
+    {
+        question: 'Вопрос 5',
+        answer: 'Ответ 5'
+    },
+    {
+        question: 'Вопрос 6',
+        answer: 'Ответ 6'
+    },
+    {
+        question: 'Вопрос 7',
+        answer: 'Ответ 7'
+    },
+    {
+        question: 'Вопрос 8',
+        answer: 'Ответ 8'
+    },
+    {
+        question: 'Вопрос 9',
+        answer: 'Ответ 9'
+    },
+    {
+        question: 'Вопрос 10',
+        answer: 'Ответ 10'
+    },
+    {
+        question: 'Вопрос 11',
+        answer: 'Ответ 11'
+    },
+    {
+        question: 'Вопрос 12',
+        answer: 'Ответ 12'
+    },
 ];
 
 
+const nextRound = (roomId) => {
+    if (!rooms[roomId]) {
+        return;
+    }
+    console.log(rooms[roomId].roundTime);
 
-let questionIndex = 0;
+    if (rooms[roomId].roundCount !== 1) {
+        rooms[roomId].currentPoints = 0;
+        rooms[roomId].roundTime -= 10;
+        kickTheWeakest(roomId);
+    }
+
+    if (rooms[roomId].players.length === 1) {
+        const totalBank = rooms[roomId].bank;
+        io.sockets.sockets.get(rooms[roomId].players[0].id).emit('won');
+        return;
+        // io.sockets.sockets.get(rooms[roomId].players[0].id).emit(
+        //     'message', `You won the bank of ${totalBank} points!`
+        // );
+    }
+
+    io.to(roomId).emit('setCurrentPlayerIndex', 0);
+    io.to(roomId).emit('setRound', rooms[roomId].roundCount);
+    io.to(roomId).emit('setTimer', rooms[roomId].roundTime);
+
+    const questionIndex = rooms[roomId].questionIndex;
+    io.to(roomId).emit('nextQuestion', questions[questionIndex].question, questionIndex);
+
+    ++rooms[roomId].roundCount;
+};
+
+const kickTheWeakest = (roomId) => {
+    if (!rooms[roomId] || !rooms[roomId].players) {
+        return;
+    }
+
+    let sorted = [].sort.call(rooms[roomId].players, function (a, b) {
+        let a_val = a.totalAnswers ? (a.correctAnswers / a.totalAnswers) : 1;
+        let b_val = b.totalAnswers ? (b.correctAnswers / b.totalAnswers) : 1;
+        // let a_val = a.correctAnswers / a.totalAnswers;
+        // let b_val = b.correctAnswers / b.totalAnswers;
+        if (a_val > b_val) { return 1 }
+        else if (a_val < b_val) { return -1 }
+        else return 0;
+    });
+    let playerToKick = sorted[0];
+
+
+    let playerToKickIndex = rooms[roomId].players.indexOf(playerToKick);
+    if (!playerToKick) {
+        playerToKickIndex = 0;
+        playerToKick = rooms[roomId].players[0];
+    }
+
+    console.log('kick:', playerToKick, 'room:', rooms[roomId].players);
+
+    rooms[roomId].players.splice(playerToKickIndex, 1);
+
+    console.log('room2:', rooms[roomId].players);
+
+    io.to(roomId).emit('setPlayers', rooms[roomId].players)
+
+    io.sockets.sockets.get(playerToKick.id).emit('kickTheWeakest', playerToKickIndex);
+    io.sockets.sockets.get(playerToKick.id).emit('message', 'You lost!');
+}
+
 const playerAnswers = {};
 const rooms = {};
+
+
 
 io.on('connection', (socket) => {
     console.log(`user connected:`, socket.id);
 
     socket.on('joinRoom', (roomId, playerName) => {
+        console.log(roomId, playerName);
         socket.join(roomId);
 
-        io.to(roomId).emit('message', `${playerName} has connected`)
+        socket.broadcast.to(roomId).emit('message', `${playerName} has connected`)
 
         if (!rooms[roomId]) {
             rooms[roomId] = {
                 players: [],
-                currentQuestion: '',
+                questionIndex: 0,
                 currentPoints: 0,
                 bank: 0,
-                roundTime: 90,
+                roundTime: 40,
+                roundCount: 1,
             }
         }
 
-        rooms[roomId].players.push({ id: socket.id, name: playerName });
+        rooms[roomId].players.push({ id: socket.id, name: playerName, correctAnswers: 0, totalAnswers: 0 });
+
+        io.to(roomId).emit('setPlayers', rooms[roomId].players)
+        console.log(rooms[roomId].players);
+        if (rooms[roomId].players.length === 4) {
+            io.to(roomId).emit('startGame');
+            nextRound(roomId)
+        }
     });
 
-    socket.on('startGame', (roomId) => {
-        if (rooms[roomId].players.length === 8) {
-            io.emit('startGame', rooms[roomId].players);
+    socket.on('nextRound', (roomId) => {
+        nextRound(roomId);
+    });
+
+
+    socket.on('checkAnswer', (roomId, currentPlayerIndex, questionIndex, answer) => {
+        if (!rooms[roomId]) {
+            return;
         }
-    })
 
+        console.log('AnswerCheck:', answer, questionIndex)
 
-    socket.on('answer', ({ roomId, questionIndex, answer }) => {
+        rooms[roomId].players[currentPlayerIndex].totalAnswers += 1;
         if (answer === questions[questionIndex].answer) {
+            rooms[roomId].players[currentPlayerIndex].correctAnswers += 1;
             if (rooms[roomId].currentPoints) {
                 rooms[roomId].currentPoints *= 2
             } else {
@@ -78,27 +191,44 @@ io.on('connection', (socket) => {
         } else {
             rooms[roomId].currentPoints = 0;
         }
+        io.to(roomId).emit('setPoints', rooms[roomId].currentPoints);
+
+        ++rooms[roomId].questionIndex;
+        questionIndex = rooms[roomId].questionIndex;
+        console.log(questionIndex);
+
+        ++currentPlayerIndex;
+        if (currentPlayerIndex === rooms[roomId].players.length) {
+            currentPlayerIndex = 0;
+        }
+        console.log('cp:', currentPlayerIndex);
+        io.to(roomId).emit('setCurrentPlayerIndex', currentPlayerIndex);
+
+        io.to(roomId).emit('nextQuestion', questions[questionIndex].question, questionIndex);
     });
+
+    // socket.on('kickTheWeakest', (roomId) => {
+    //
+    // });
 
     socket.on('bank', (roomId) => {
         rooms[roomId].bank += rooms[roomId].currentPoints;
         rooms[roomId].currentPoints = 0;
+        io.to(roomId).emit('setPoints', 0);
+        io.to(roomId).emit('setBank', rooms[roomId].bank);
     });
 
 
     socket.on('disconnect', () => {
+        for (const roomId in rooms) {
+            rooms[roomId].players = rooms[roomId].players.filter(
+                (playerName) => playerName.id !== socket.id
+            );
+        }
+
         console.log(`user disconnected:`, socket.id)
     });
 });
-
-const nextQuestion = () => {
-    if (questionIndex < questions.length) {
-        io.emit('question', questions[questionIndex]);
-        questionIndex++;
-    } else {
-        io.emit('gameOver', playerAnswers);
-    }
-};
 
 server.listen(3001, () => {
     console.log(`Server is running on port ${3001}`);
